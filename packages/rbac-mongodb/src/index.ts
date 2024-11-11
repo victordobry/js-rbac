05a6a5ece85c43fa7a644e416ad01dfb33038823
Victor Dombrovskiy <dombrovskiy@klaim.ai>
2024-11-12 01:11:42 +0700

    Do not export models (they are internals).

--- packages/rbac-mongodb/src/index.ts
+++ packages/rbac-mongodb/src/index.ts
@@ -4,19 +4,10 @@ import RbacMongodbItemAdapter from './adapters/RbacMongodbItemAdapter';
 import RbacMongodbItemChildAdapter from './adapters/RbacMongodbItemChildAdapter';
 import RbacMongodbRuleAdapter from './adapters/RbacMongodbRuleAdapter';
 
-import RbacAssignment from './models/RbacAssignment';
-import RbacItem from './models/RbacItem';
-import RbacItemChild from './models/RbacItemChild';
-import RbacRule from './models/RbacRule';
-
 export {
   RbacMongodbAdapter,
   RbacMongodbAssignmentAdapter,
   RbacMongodbItemAdapter,
   RbacMongodbItemChildAdapter,
-  RbacMongodbRuleAdapter,
-  RbacAssignment,
-  RbacItem,
-  RbacItemChild,
-  RbacRule
+  RbacMongodbRuleAdapter
 };
--- packages/rbac-postgres/src/index.ts
+++ packages/rbac-postgres/src/index.ts
@@ -4,19 +4,10 @@ import RbacPostgresItemAdapter from './adapters/RbacPostgresItemAdapter';
 import RbacPostgresItemChildAdapter from './adapters/RbacPostgresItemChildAdapter';
 import RbacPostgresRuleAdapter from './adapters/RbacPostgresRuleAdapter';
 
-import RbacAssignment from './models/RbacAssignment';
-import RbacItem from './models/RbacItem';
-import RbacItemChild from './models/RbacItemChild';
-import RbacRule from './models/RbacRule';
-
 export {
   RbacPostgresAdapter,
   RbacPostgresAssignmentAdapter,
   RbacPostgresItemAdapter,
   RbacPostgresItemChildAdapter,
-  RbacPostgresRuleAdapter,
-  RbacAssignment,
-  RbacItem,
-  RbacItemChild,
-  RbacRule
+  RbacPostgresRuleAdapter
 };
